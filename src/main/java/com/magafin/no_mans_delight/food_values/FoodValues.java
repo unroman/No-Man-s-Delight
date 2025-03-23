9311218353e2007b99d2393aee86b76efd219066
Magafin <magafiny@gmail.com>
2025-03-23 18:19:43 +0200

    No Mans Delight

---
+++ src/main/java/com/magafin/no_mans_delight/food_values/FoodValues.java
@@ -0,0 +1,53 @@
+package com.magafin.no_mans_delight.food_values;
+
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import vectorwing.farmersdelight.common.registry.ModEffects;
+
+import java.util.Map;
+
+public class FoodValues {
+    public static final int BRIEF_DURATION = 600;    // 30 seconds
+    public static final int SHORT_DURATION = 1200;    // 1 minute
+    public static final int MEDIUM_DURATION = 3600;    // 3 minutes
+    public static final int LONG_DURATION = 6000;// 5 minutes
+
+    public static final FoodProperties STUFFED_VENISON = (new FoodProperties.Builder())
+            .nutrition(7).saturationModifier(0.8f)
+            .effect(() -> new MobEffectInstance(ModEffects.COMFORT, MEDIUM_DURATION, 0), 1.0F).build();
+    public static final FoodProperties HONEY_GLAZED_BILLHOOK_BASS = (new FoodProperties.Builder())
+            .nutrition(12).saturationModifier(0.8f)
+            .effect(() -> new MobEffectInstance(ModEffects.NOURISHMENT, LONG_DURATION, 0), 1.0F).build();
+    public static final FoodProperties HORSE_STEAK_WITH_FRUITS = (new FoodProperties.Builder())
+            .nutrition(9).saturationModifier(0.8f)
+            .effect(() -> new MobEffectInstance(ModEffects.COMFORT, SHORT_DURATION, 0), 1.0F).build();
+    public static final FoodProperties MUSHROOMS_WITH_TOMATO = (new FoodProperties.Builder())
+            .nutrition(11).saturationModifier(0.8f)
+            .effect(() -> new MobEffectInstance(ModEffects.NOURISHMENT, LONG_DURATION, 0), 1.0F).build();
+    public static final FoodProperties SWEET_FROG_LEGS = (new FoodProperties.Builder())
+            .nutrition(8).saturationModifier(0.8f)
+            .effect(() -> new MobEffectInstance(ModEffects.COMFORT, MEDIUM_DURATION, 0), 1.0F).build();
+    public static final FoodProperties VENISON_TART = (new FoodProperties.Builder())
+            .nutrition(10).saturationModifier(0.8f)
+            .effect(() -> new MobEffectInstance(ModEffects.NOURISHMENT, LONG_DURATION, 0), 1.0F).build();
+    public static final FoodProperties VENISON_ROULADE = (new FoodProperties.Builder())
+            .nutrition(12).saturationModifier(0.8f)
+            .effect(() -> new MobEffectInstance(ModEffects.NOURISHMENT, LONG_DURATION, 0), 1.0F).build();
+    public static final FoodProperties SHROOMBURGER = (new FoodProperties.Builder())
+            .nutrition(4).saturationModifier(0.6f).build();
+    public static final FoodProperties HORSE_SANDWICH = (new FoodProperties.Builder())
+            .nutrition(8).saturationModifier(0.7f).build();
+    public static final FoodProperties HORSE_LOIN = (new FoodProperties.Builder())
+            .nutrition(2).saturationModifier(0.3f).build();
+    public static final FoodProperties COOKED_HORSE_LOIN = (new FoodProperties.Builder())
+            .nutrition(5).saturationModifier(0.6f).build();
+    public static final FoodProperties VENISON_MEDALLIONS = (new FoodProperties.Builder())
+            .nutrition(1).saturationModifier(0.3f).build();
+    public static final FoodProperties COOKED_VENISON_MEDALLIONS = (new FoodProperties.Builder())
+            .nutrition(3).saturationModifier(0.6f).build();
+}
+
---
+++ src/main/java/com/magafin/no_mans_delight/nmdReg.java
@@ -0,0 +1,58 @@
+package com.magafin.no_mans_delight;
+
+import com.magafin.no_mans_delight.food_values.FoodValues;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import vectorwing.farmersdelight.common.item.ConsumableItem;
+
+import static com.magafin.no_mans_delight.No_mans_delight.MODID;
+
+public class nmdReg {
+    public static Item.Properties foodItem(FoodProperties food) {
+        return new Item.Properties().food(food);
+    }
+    public static Item.Properties bigFoodItem(FoodProperties food) {
+        return new Item.Properties().food(food).stacksTo(32);
+    }
+    public static Item.Properties bowlFoodItem(FoodProperties food) {
+        return new Item.Properties().food(food).craftRemainder(Items.BOWL).stacksTo(16);
+    }
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+    public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(Registries.ENTITY_TYPE, MODID);
+
+    public static final DeferredItem<Item> STUFFED_VENISON = ITEMS.register("stuffed_venison", registryName ->
+            new ConsumableItem(bowlFoodItem(FoodValues.STUFFED_VENISON), true));
+    public static final DeferredItem<Item> HONEY_GLAZED_BILLHOOK_BASS = ITEMS.register("honey_glazed_billhook_bass", registryName ->
+            new ConsumableItem(bowlFoodItem(FoodValues.HONEY_GLAZED_BILLHOOK_BASS), true));
+    public static final DeferredItem<Item> HORSE_STEAK_WITH_FRUITS = ITEMS.register("horse_steak_with_fruits", registryName ->
+            new ConsumableItem(bowlFoodItem(FoodValues.HORSE_STEAK_WITH_FRUITS), true));
+    public static final DeferredItem<Item> MUSHROOMS_WITH_TOMATO = ITEMS.register("mushrooms_with_tomato", registryName ->
+            new ConsumableItem(bowlFoodItem(FoodValues.MUSHROOMS_WITH_TOMATO), true));
+    public static final DeferredItem<Item> SWEET_FROG_LEGS = ITEMS.register("sweet_frog_legs", registryName ->
+            new ConsumableItem(bowlFoodItem(FoodValues.SWEET_FROG_LEGS), true));
+    public static final DeferredItem<Item> VENISON_TART = ITEMS.register("venison_tart", registryName ->
+            new ConsumableItem(bowlFoodItem(FoodValues.VENISON_TART), true));
+    public static final DeferredItem<Item> SHROOMBURGER = ITEMS.register("shroomburger", registryName ->
+            new ConsumableItem(foodItem(FoodValues.SHROOMBURGER), false));
+    public static final DeferredItem<Item> HORSE_SANDWICH = ITEMS.register("horse_sandwich", registryName ->
+            new ConsumableItem(bigFoodItem(FoodValues.HORSE_SANDWICH), false));
+    public static final DeferredItem<Item> HORSE_LOIN = ITEMS.register("horse_loin", registryName ->
+            new ConsumableItem(foodItem(FoodValues.HORSE_LOIN), false));
+    public static final DeferredItem<Item> COOKED_HORSE_LOIN = ITEMS.register("cooked_horse_loin", registryName ->
+            new ConsumableItem(foodItem(FoodValues.COOKED_HORSE_LOIN), false));
+    public static final DeferredItem<Item> VENISON_MEDALLIONS = ITEMS.register("venison_medallions", registryName ->
+            new ConsumableItem(foodItem(FoodValues.VENISON_MEDALLIONS), false));
+    public static final DeferredItem<Item> COOKED_VENISON_MEDALLIONS = ITEMS.register("cooked_venison_medallions", registryName ->
+            new ConsumableItem(foodItem(FoodValues.COOKED_VENISON_MEDALLIONS), false));
+    public static final DeferredItem<Item> VENISON_ROULADE = ITEMS.register("venison_roulade", registryName ->
+            new ConsumableItem(foodItem(FoodValues.VENISON_ROULADE), true));
+}
+
